1b2d6540b0cba93b3c2eba079289d2246dbf3b7f
ackenney <ackenney@gmail.com>
2021-09-21 15:34:06 -0400

    added statements to initialize the sum and calculate the sum within the for loop

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 // Implement the body of the function below & ensure it passes the provided unit tests by running npm run test.
 const sum = (arrayOfNumbers) => {
 
+  let sum = 0; //initialize sum 
+
   //For loop to iterate through the array
   for(let i = 0; i < arrayOfNumbers.length; i++){
+      sum = sum + arrayOfNumbers[i]; // calculating sum
   }
-  
+
 };
 
 module.exports = {
