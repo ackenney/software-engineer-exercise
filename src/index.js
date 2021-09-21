98ff5d6585ae23cfdce44f4c236086e2374b430c
ackenney <ackenney@gmail.com>
2021-09-21 15:28:48 -0400

    added a for loop to iterate through the array

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 // Implement the body of the function below & ensure it passes the provided unit tests by running npm run test.
 const sum = (arrayOfNumbers) => {
-  throw new Error('Not Implemented.');
+
+  //For loop to iterate through the array
+  for(let i = 0; i < arrayOfNumbers.length; i++){
+  }
+  
 };
 
 module.exports = {
